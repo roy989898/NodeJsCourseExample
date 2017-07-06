12fef5c109be789eb0834061d448e7f79acdc86d
poming\pomingpo <roy989898@gmail.com>
2017-07-06 12:52:16 +0800

    yargs module install

--- notes_node/app.js
+++ notes_node/app.js
@@ -6,19 +6,3 @@ const os = require('os');
 const notes = require('./note')
 const _ = require("lodash");
 
-/*let user = os.userInfo();
-// console.log(user);
-let userName = user.username;
-fs.appendFile('greetings.txt', "Hello " + userName + " You are " + notes.age);
-
-console.log("finish app");
-*/
-
-/*let res = notes.addNote();
-console.log(res);
-
-let result = notes.add(1, 2);
-
-console.log(result);*/
-
-console.log(_.isNumber("44"));
