e25eca6ec914c54f2c0cc98499f7b4a82fdab78d
poming\pomingpo <roy989898@gmail.com>
2017-07-06 14:40:40 +0800

    bug fix

--- notes_node/app.js
+++ notes_node/app.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
 
-const notes = require('./notes.js');
+const notes = require('./note.js');
 
 const argv = yargs.argv;
 var command = argv._[0];
