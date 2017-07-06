3c3cc52e6e7e47377f81bea91b85351176755af2
poming\pomingpo <roy989898@gmail.com>
2017-07-06 12:05:51 +0800

    can export our note.js

--- notes_node/app.js
+++ notes_node/app.js
@@ -3,10 +3,19 @@
 console.log("starting app");
 const fs = require('fs');
 const os = require('os');
+const notes = require('./note')
 
-let user = os.userInfo();
-console.log(user);
+/*let user = os.userInfo();
+// console.log(user);
 let userName = user.username;
-fs.appendFile('greetings.txt', "Hello " + userName);
+fs.appendFile('greetings.txt', "Hello " + userName + " You are " + notes.age);
 
 console.log("finish app");
+*/
+
+let res = notes.addNote();
+console.log(res);
+
+let result = notes.add(1, 2);
+
+console.log(result);
---
+++ notes_node/note.js
@@ -0,0 +1,12 @@
+console.log("Starting note.js");
+console.log(module);
+module.exports.addNote = () => {
+    console.log("addNote");
+    return "New note";
+
+};
+
+module.exports.add = (num1, num2) => {
+    return num1 + num2;
+
+}
