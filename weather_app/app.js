b12fe4a218057e0d155d54bc1522f15b03e823ae
poming\pomingpo <roy989898@gmail.com>
2017-07-07 15:15:04 +0800

    improve the showing the response body

--- weather_app/app.js
+++ weather_app/app.js
@@ -9,5 +9,5 @@ request({
 }, function (error, respponse, body) {
     // console.log('error:', error); // Print the error if one occurred 
     // console.log('statusCode:', respponse && respponse.statusCode); // Print the response status code if a response was received 
-    console.log('body:', body); // Print the HTML for the Google hom
+    console.log('body:', JSON.stringify(body, undefined, 3)); // Print the HTML for the Google hom
 });
