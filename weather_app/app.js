6a9fcbcadf89ef2f2df2c2b25f08397ace1273cf
poming\pomingpo <roy989898@gmail.com>
2017-07-07 15:08:20 +0800

    app.js can request the geo information

---
+++ weather_app/app.js
@@ -0,0 +1,13 @@
+const request = require("request");
+let address = "103";
+const requestLink = "https://maps.googleapis.com/maps/api/geocode/json?address=" +
+    address;
+request({
+    url: requestLink,
+    json: true
+
+}, function (error, respponse, body) {
+    // console.log('error:', error); // Print the error if one occurred 
+    // console.log('statusCode:', respponse && respponse.statusCode); // Print the response status code if a response was received 
+    console.log('body:', body); // Print the HTML for the Google hom
+});
