c488655f4b92d56d7d42e15bb65eb8d944a1a745
poming\pomingpo <roy989898@gmail.com>
2017-07-08 09:36:42 +0800

    refactoring

--- weather_app/app.js
+++ weather_app/app.js
@@ -1,4 +1,4 @@
-const request = require("request");
+
 const argv = require('yargs').options({
     a: {
         demand: true,
@@ -9,26 +9,9 @@ const argv = require('yargs').options({
 }).help()
     .alias('help', 'h').argv;
 
-let address;
-if (argv.address) {
-    address = argv.address;
-} else {
-    address = "elegance garden";
-}
 
-const requestLink = "https://maps.googleapis.com/maps/api/geocode/json?address=" +
-    encodeURIComponent(address);
-request({
-    url: requestLink,
-    json: true
 
-}, function (error, respponse, body) {
-    if (error || body.results.length < 1) {
-        console.log('Error:', "Nothing can search");
-    } else {
-        console.log('Address:', JSON.stringify(body.results[0].formatted_address));
-        console.log('Latitude:', JSON.stringify(body.results[0].geometry.location.lat));
-        console.log('Longitude:', JSON.stringify(body.results[0].geometry.location.lng));
-    }
+const geocode = require("./gecode/gecode");
 
-});
+geocode.gecodeAddress(argv.address);
+
---
+++ weather_app/gecode/gecode.js
@@ -0,0 +1,25 @@
+
+const request = require("request");
+let gecodeAddress = (address) => {
+
+    const requestLink = "https://maps.googleapis.com/maps/api/geocode/json?address=" +
+        encodeURIComponent(address);
+    request({
+        url: requestLink,
+        json: true
+
+    }, function (error, respponse, body) {
+        if (error || body.results.length < 1) {
+            console.log('Error:', "Nothing can search");
+        } else {
+            console.log('Address:', JSON.stringify(body.results[0].formatted_address));
+            console.log('Latitude:', JSON.stringify(body.results[0].geometry.location.lat));
+            console.log('Longitude:', JSON.stringify(body.results[0].geometry.location.lng));
+        }
+
+    });
+
+}
+
+
+module.exports = { gecodeAddress };
