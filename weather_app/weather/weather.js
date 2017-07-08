2d1976ddc6afd9edcf0c4a26838bd9e18793c616
poming\pomingpo <roy989898@gmail.com>
2017-07-08 10:22:55 +0800

    can show the weather

--- weather_app/weather/weather.js
+++ weather_app/weather/weather.js
@@ -7,9 +7,12 @@ let queryCurrentWeather = (lat, long) => {
         url: requestLink,
         json: true
     }, function (error, respponse, body) {
-        if (error) {
+        if (error || respponse.statusCode != 200) {
             console.log("Getting weather Error");
         } else {
+            let bodyString = JSON.stringify(body, undefined, 2);
+            console.log(bodyString);
+
 
         }
     });
