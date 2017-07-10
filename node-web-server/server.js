4f29eb3de65c76f85f1465ad7f2d29e523ea9e35
poming\pomingpo <roy989898@gmail.com>
2017-07-10 14:45:39 +0800

    add iddware to log the request

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 let app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 
 
 app.set('view engine', 'hbs');
+
+// MiddleWARE
 app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = now + ": " + req.method + ' ' + req.url;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append the log');
+        }
+    });
+    next();//tell when the middware function done
+
+});
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
