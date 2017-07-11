de2b9dc3b28935165b239d4052bb160eb6ee61f6
poming\pomingpo <roy989898@gmail.com>
2017-07-11 14:55:06 +0800

    add test

--- node-test/utils/utils.js
+++ node-test/utils/utils.js
@@ -3,3 +3,11 @@ module.exports.add = (a, b) => a + b;
 module.exports.square = (x) => {
     return x * x;
 };
+
+module.exports.setName = (user, fullName) => {
+    let names = fullName.split(" ");
+    user.firstName = names[0];
+    user.lastName = names[1];
+
+    return user;
+};
--- node-test/utils/utils.test.js
+++ node-test/utils/utils.test.js
@@ -1,17 +1,46 @@
+
+const expect = require("expect");
 const utils = require("./utils");
 
 it("should add two numbers", () => {
     let res = utils.add(33, 11);
-    if (res !== 44) {
-        throw new Error("Expected 44, but got " + res);
-    }
 
+    expect(res).toBe(44).toBeA("number");
 });
 
 it("should square the number", () => {
     let res = utils.square(4);
-    if (res !== 16) {
-        throw new Error("Expected 16, but got " + res);
-    }
+
+    expect(res).toBe(16).toBeA("number");
+
+});
+
+// should verify firstand last names are set
+// assert it includes firstName and lastName with proper values
+it("should set firstName and lastName", () => {
+    let user = { location: "HK", age: 25 };
+    let res = utils.setName(user, "Roy Leung");
+    expect(res).toInclude({
+        firstName: "Roy",
+        lastName: "Leung"
+
+    });
+
+
+});
+
+
+
+it("should expect some values", () => {
+    // expect(12).tonotBe(12);
+    // expect({ name: "Andrew" }).toNotEqual({ name: "Andrew" });
+    // expect([2, 3, 4]).toInclude(5);
+    expect({
+        name: "andrew",
+        age: 25,
+        location: "HK"
+    }).toInclude({
+        age: 25
+    });
 
 });
