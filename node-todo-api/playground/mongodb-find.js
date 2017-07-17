8a65aed2c923d00eca44ac52aada9e48ddc37afa
poming\pomingpo <roy989898@gmail.com>
2017-07-17 16:49:44 +0800

    update

--- node-todo-api/playground/mongodb-find.js
+++ node-todo-api/playground/mongodb-find.js
@@ -5,28 +5,28 @@ MongoClient.connect('mongodb://localhost/TodoApp', (err, db) => {
         return console.log('unable to connect toMongoDb');
     }
     // query all
-    /*  console.log('connect toMongoDb');
-     db.collection('Todos').find().toArray().then((docs) => {
-         console.log('Todos');
-         console.log(JSON.stringify(docs, undefined, 2));
-     }, (err) => {
-         console.log('unable to fetch todos', err);
-     }); */
+    console.log('connect toMongoDb');
+    db.collection('Todos').find().toArray().then((docs) => {
+        console.log('Todos');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('unable to fetch todos', err);
+    });
 
     //  query only the document contain the completed: false value
-    /*   db.collection('Todos').find({ _id: new ObjectID('596c32e2ce9d9d04e8a68244') }).toArray().then((docs) => {
-          console.log('Todos');
-          console.log(JSON.stringify(docs, undefined, 2));
-      }, (err) => {
-          console.log('unable to fetch todos', err);
-      }); */
+    db.collection('Todos').find({ _id: new ObjectID('596c32e2ce9d9d04e8a68244') }).toArray().then((docs) => {
+        console.log('Todos');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('unable to fetch todos', err);
+    });
 
     //   count the doucment number
-    /*  db.collection('Todos').find().count().then((count) => {
-         console.log('Todos count', count);
-     }, (err) => {
-         console.log('unable to fetch todos', err);
-     }); */
+    db.collection('Todos').find().count().then((count) => {
+        console.log('Todos count', count);
+    }, (err) => {
+        console.log('unable to fetch todos', err);
+    });
 
     db.collection('Users').find({ name: 'Tom' }).toArray().then((docs) => {
         console.log(JSON.stringify(docs, undefined, 2));
