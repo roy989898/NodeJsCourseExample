83a8c4d747866e7810f2db65d8470300c97474bf
poming\pomingpo <roy989898@gmail.com>
2017-07-17 12:21:26 +0800

    use mongo db

---
+++ node-todo-api/playground/mongodb-connect.js
@@ -0,0 +1,30 @@
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb://localhost/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('unable to connect toMongoDb');
+    }
+    console.log('connect toMongoDb');
+    /*  db.collection('Todos').insertOne({
+         text: 'Something to do',
+         completed: false
+     }, (err, result) => {
+ 
+         if (err) {
+             return console.log('unable to insert');
+         }
+         console.log(JSON.stringify(result.ops, undefined, 2));
+     }); */
+
+    db.collection('Users').insertOne({
+        name: 'Andrew',
+        age: 25,
+        location: 'HK'
+    }, (err, result) => {
+        if (err) {
+            return console.log('Unable to insert user');
+        }
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    });
+
+    db.close();
+});
