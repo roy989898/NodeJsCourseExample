54cac97e47c0b8ebe032b1048aebeec666891bd2
poming\pomingpo <roy989898@gmail.com>
2017-07-24 10:46:07 +0800

    add library

---
+++ node-todo-api/typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="modules/express/index.d.ts" />
+/// <reference path="modules/lodash/index.d.ts" />
+/// <reference path="modules/mongodb/index.d.ts" />
+/// <reference path="modules/mongoose/index.d.ts" />
