e34c64b3123e425c208083d65e046691b81d3737
poming\pomingpo <roy989898@gmail.com>
2017-07-24 12:32:40 +0800

    install body parser typings

--- node-todo-api/typings/index.d.ts
+++ node-todo-api/typings/index.d.ts
@@ -1,4 +1,6 @@
+/// <reference path="globals/mongoose/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="modules/body-parser/index.d.ts" />
 /// <reference path="modules/express/index.d.ts" />
 /// <reference path="modules/lodash/index.d.ts" />
 /// <reference path="modules/mongodb/index.d.ts" />
