88fbb7775ea6eb7ace408eb9c18d9c4b36a8ea5a
poming\pomingpo <roy989898@gmail.com>
2017-07-24 17:01:45 +0800

    add get  /todos

--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -6,8 +6,17 @@ const app = require('../server').app;
 const Todo = require('../models/todo').Todo;
 const User = require('../models/user').Todo;
 
+const todos = [{
+    text: 'First test todo'
+}, {
+    text: 'Second test todo'
+}];
+// delete all the Todo
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => { done(); });
+
 });
 
 
@@ -29,14 +38,46 @@ describe('POST /todos', () => {
                 }
 
                 // check the database is correct
-                Todo.find().then((todos) => {
-                    expect(todos.length).toBe(2);
-                    console.log('finsih length');
+                Todo.find({ text }).then((todos) => {
+                    expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
-                    console.log('finsih text');
                     done();
                 }).catch((e) => done(e));
             });
     });
 
+    it('should not create todo with invalid body data', (done) => {
+
+        request(app)
+            .post('/todos')
+            .send({})
+            .expect(400)
+            .end((err, res) => {
+
+                if (err) {
+                    return done(err);
+                }
+                Todo.find().then((todos) => {
+                    expect(todos.length).toBe(2);
+                    done();
+
+                }).catch((e) => { done(e); });
+            });
+    });
+
+});
+
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .send()
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+
+    });
 });
