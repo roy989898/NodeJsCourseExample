eb059f0ce8215ac3f4150dd39904e2833847a764
roy989898 <roy989898@gmail.com>
2017-07-25 17:15:24 +0800

    chnage the return

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -45,12 +45,12 @@ app.get('/todos/:id', (req, res) => {
         Todo.findById(id).then((todo) => {
 
             if (!todo) {
-                return res.status(404).send({text:'cant find'});
+                return res.status(400).send({});
             }
             res.send({todo});
 
         }, (e) => {
-            res.status(404).send({text:'error'});
+            res.status(404).send({});
         });
 
     }
