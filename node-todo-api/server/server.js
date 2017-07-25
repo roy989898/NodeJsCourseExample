9f5e17d4eec3c029b6908a791369628fd7b56d24
roy989898 <roy989898@gmail.com>
2017-07-25 17:13:48 +0800

    add query by id

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,6 +1,7 @@
 
 let Todo = require('./models/todo').Todo;
 let User = require('./models/user').User;
+const ObjectID = require('mongodb').ObjectID;
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -28,9 +29,35 @@ app.get('/todos', (req, res) => {
     Todo.find({}).then((todos) => {
         res.send({ todos });
     }, (e) => {
-        res.status(400).send(e);
+        res.status(404).send(e);
     });
 });
+
+
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    // valid id use is Vaild
+    if (!ObjectID.isValid(id)) {
+        res.status(400).send({});
+    } else {
+        Todo.findById(id).then((todo) => {
+
+            if (!todo) {
+                return res.status(404).send({text:'cant find'});
+            }
+            res.send({todo});
+
+        }, (e) => {
+            res.status(404).send({text:'error'});
+        });
+
+    }
+
+});
+
+
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
