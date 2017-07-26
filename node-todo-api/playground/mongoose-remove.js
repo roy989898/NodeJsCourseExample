c375b78d521387d5677891342239071cd8f39569
roy989898 <roy989898@gmail.com>
2017-07-26 12:53:43 +0800

    try findByIdAndRemove

--- node-todo-api/playground/mongoose-remove.js
+++ node-todo-api/playground/mongoose-remove.js
@@ -3,7 +3,12 @@ const ObjectID = require('mongodb').ObjectID;
 const Todo = require('../server/models/todo').Todo;
 const User = require('../server/models/user').User;
 
-Todo.remove({}).then((result) => {
+/* Todo.remove({}).then((result) => {
     console.log(result);
-});
+}); */
 
+
+
+Todo.findByIdAndRemove('59781fedf8a658b6279a22d0').then(todo => {
+    console.log(todo);
+});
