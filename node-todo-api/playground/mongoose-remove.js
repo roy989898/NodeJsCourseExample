9eb11da45a34714052845b00ff4a6076182aa555
roy989898 <roy989898@gmail.com>
2017-07-26 12:45:08 +0800

    try moogose remove

--- node-todo-api/playground/mongoose-remove.js
+++ node-todo-api/playground/mongoose-remove.js
@@ -1,28 +1,9 @@
 const momgoose = require('../server/db/mongoose').mongoose;
 const ObjectID = require('mongodb').ObjectID;
 const Todo = require('../server/models/todo').Todo;
+const User = require('../server/models/user').User;
 
+Todo.remove({}).then((result) => {
+    console.log(result);
+});
 
-let id = '5975e1bb8fddddc90720c814d0d7';
-
-
-if (!ObjectID.isValid(id)) {
-    console.log('ID is not valid');
-}
-
-Todo.find({
-    _id: id
-}).then((todos) => {
-    console.log('Todos: ', todos);
-}, (e) => { console.log(e); });
-
-Todo.findOne({
-    _id: id
-}).then((todo) => {
-    console.log('Todo: ', todo);
-}, (e) => { console.log(e); });
-
-
-Todo.findById(id).then((todo) => {
-    console.log('Todo by id: ', todo);
-}, (e) => { console.log(e); });
