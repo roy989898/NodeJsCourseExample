065a6d9372c57501b40e9f600c219c4eef39e2b2
roy989898 <roy989898@gmail.com>
2017-07-27 17:28:19 +0800

    bug fix

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -26,12 +26,12 @@ app.post('/todos', (req, res) => {
 
 });
 
-app.get('/todos', (req, res) => {
+app.get('/cap', (req, res) => {
     hbs.render('cap.hbs');
    
 });
 
-app.get('/cap', (req, res) => {
+app.get('/todos', (req, res) => {
     Todo.find({}).then((todos) => {
         res.send({ todos });
     }, (e) => {
