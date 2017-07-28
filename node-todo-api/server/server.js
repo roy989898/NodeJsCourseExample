9864ffe3b02d2fff92d85fe85a60e84774503ae0
roy989898 <roy989898@gmail.com>
2017-07-28 12:29:08 +0800

    add the env

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,4 +1,16 @@
 
+let env = process.env.NODE_ENV || 'development';
+console.log('env  ******', env);
+
+
+if (env === 'development') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if (env === 'test') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}
+
 const _ = require('lodash');
 let Todo = require('./models/todo').Todo;
 let User = require('./models/user').User;
