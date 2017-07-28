7b81185114aabd53b43316c79f2352c8a9d968d3
roy989898 <roy989898@gmail.com>
2017-07-28 15:00:09 +0800

    create the scheme for the user

--- node-todo-api/server/models/user.js
+++ node-todo-api/server/models/user.js
@@ -1,14 +1,46 @@
 
-let mongoose = require('../db/mongoose').mongoose;
+const mongoose = require('../db/mongoose').mongoose;
+const validator = require('validator');
+/* {
+    email:'sadas@asdas.com',
+    password:'myPass123has',
+    tokens:[
+        {
+            access:'auth',
+            token:'sadasdsad'
+        }
+    ]
+} */
 let User = mongoose.model('User', {
     email: {
         type: String,
         required: true,
         trim: true,
-        minlength: 1
+        minlength: 1,
+        unique: true,
+        validate: {
+            validator: (value) => {
+                return validator.isEmail(value);
+            },
+            message: '{VALUE} is not a valid email'
+        }
+    },
 
-
-    }
+    password: {
+        type: Strinh,
+        require: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
 module.exports = {
