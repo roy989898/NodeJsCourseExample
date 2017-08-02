935f681d8c8f8da803777fbd4a39878dc56c9d79
roy989898 <roy989898@gmail.com>
2017-08-02 14:20:18 +0800

    add Logging Out - DELETE /users/me/token

--- node-todo-api/server/models/user.js
+++ node-todo-api/server/models/user.js
@@ -61,6 +61,19 @@ UserSchema.methods.generateAuthTokenAndSave = function () {
 
 };
 
+UserSchema.methods.removeToken = function (token) {
+    let user = this;
+
+    return user.update({
+        $pull: {
+            tokens: {
+                token
+            }
+        }
+    });
+
+};
+
 UserSchema.statics.findByToken = function (token) {
     let User = this;
     let decoded;
